c32a19532647b068c30422bbb704fe044782ed18
Matthew <makolodzi@gmail.com>
2016-02-03 13:09:25 -0500

    Add 3 more cats and create cat buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 var cat1 = document.getElementById("cat1");
 var cat2 = document.getElementById("cat2");
 
@@ -42,3 +43,77 @@ cat2.addEventListener("click", function() {
 	clickNumber2.innerHTML = num2;
 });
 
+
+elem.addEventListener('click', (function(numCopy) {
+    return function() {
+        alert(numCopy);
+    };
+})(num));
+*/
+
+
+
+/*function createButtons(array){
+	buttons = [];
+	for (var i = 0; i < array.length; i++){
+		newButton = document.createElement("button");
+		newButton.type = "button";
+		newButton.textContent = array[i].name;
+		buttons.push(newButton);
+	}
+	return buttons;
+}
+*/
+
+var catList = [];
+
+//change cat and his image url into a Cat object and add it to catList
+function addCat(name, url){
+ 	var addThis = new Cat(name, url);
+ 	catList.push(addThis);
+}
+
+//Cat constructor
+function Cat (name, url){
+	this.name = name;
+	this.url = url;
+} 
+
+addCat("Fred", "images/cat.jpg");
+addCat("Mittens", "images/cat2.jpg");
+addCat("Belleh", "images/cat3.jpg");
+addCat("Cotton", "images/cat3.jpg");
+addCat("William", "images/cat3.jpg");
+
+//input catList and return a list of strings as if they are buttons in HTML format
+function buttonReady(array){
+	buttons = [];
+	for (var i = 0; i < array.length; i++){
+		newButton = "<button type=\"button\">" + array[i].name + "</button>";
+		buttons.push(newButton);
+	}
+	return buttons;
+}
+
+var catButtons = buttonReady(catList);
+
+//input list of button ready strings and return an HTML list
+function createUlList (array){
+	firstEl = document.createElement('ul');
+	for (var i = 0; i < array.length; i++){
+		listItem = document.createElement('li');
+		listItem.innerHTML = array[i];
+		firstEl.appendChild(listItem);
+	}
+	return firstEl;
+}
+
+catList = createUlList(catButtons);
+
+//input list and the id you want to insert list into
+function insertList(list, id){
+	var parent = document.getElementById(id);
+	parent.appendChild(list);
+}
+
+insertList(catList, 'catButtons');
