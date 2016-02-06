f862a7f15dd05546379ce130bef512a860ec1bfc
Matthew <makolodzi@gmail.com>
2016-02-05 20:57:27 -0500

    Get MVC Javascript program working

--- js/main2.js
+++ js/main2.js
@@ -7,21 +7,28 @@ var model = {
 		this.url = url;
 		this.clickNumber = 0;
 		this.imageId = name.toLowerCase() + "Pic";
-	}
+	},
+
+	//add cat to model.catList by using model.Cat constructor
+	addCat: function(name, url){
+	 	var addThis = new model.Cat(name, url);
+	 	model.catList.push(addThis);
+	},
 };
 
 
 /* ======= Controller ======= */
 
 var controller = {
-	//add cat to model.catList by using model.Cat constructor
-	addCat: function(name, url){
-	 	var addThis = new model.Cat(name, url);
-	 	model.catList.push(addThis);
+
+	init: function(){
+		view.listRender();
+		view.imageAndClickRender();
 	},
 
 	//input catList and return an array of strings as if they are buttons in HTML format
 	buttonReady: function(array){
+		buttons = [];
 		for (var i = 0; i < array.length; i++){
 			newButton = "<button type=\"button\">" + array[i].name + "</button>";
 			buttons.push(newButton);
@@ -35,22 +42,32 @@ var controller = {
 		for (var i = 0; i < array.length; i++){
 			listItem = document.createElement('li');
 			listItem.innerHTML = array[i];
-			name = catList[i].name;
-			listItem.id = catList[i].name;
+			listItem.id = model.catList[i].name;
 			firstEl.appendChild(listItem);
 		}
 		return firstEl;		
-	}
+	},
+
+	increaseClickCount: function(cat){
+		var number = cat.clickNumber;
+		number ++;
+		cat.clickNumber = number;
+	},
 };
 
 
 /* ======= View ======= */
 
 var view = {
+
+	catButtons: document.getElementById('catButtons'),
+	catImage: document.getElementsByClassName("catImage")[0],
+	clickTitle: document.getElementById("nameInsert"),
+	clickCount: document.getElementById("clickCount"),
+
 	//input html list and the id you want to insert list into
-	insertList: function(list, id){
-		var parent = document.getElementById(id);
-		parent.appendChild(list);	
+	insertList: function(list, idNode){
+		idNode.appendChild(list);	
 	},
 
 	createButtonHandlers: function(array){
@@ -61,11 +78,11 @@ var view = {
 				id.addEventListener("click", (function() {
 					picTitle = document.getElementById("catNameTitle");
 					picTitle.innerHTML = name;
-					catImage.src = array[j].url;
-					catImage.id = array[j].imageId;
-					var clickTitle = document.getElementById("nameInsert");
-					clickTitle.innerHTML = name;
-					clickCount.innerHTML = array[j].clickNumber;
+					view.catImage.src = array[j].url;
+					view.catImage.id = array[j].imageId;
+					//var clickTitle = document.getElementById("nameInsert");
+					view.clickTitle.innerHTML = name;
+					view.clickCount.innerHTML = array[j].clickNumber;
 				}));
 			}(i));
 		}
@@ -75,43 +92,40 @@ var view = {
 		catImage.addEventListener("click", function(){
 			var id = (catImage.id);
 			var i;
-			for (i in catList){
-				if (id === catList[i].imageId){
-					catList[i].clickNumber += 1;
-					clickCount.innerHTML = catList[i].clickNumber;
+			for (i in model.catList){
+				if (id === model.catList[i].imageId){
+					controller.increaseClickCount(model.catList[i]);
+					view.renderClickCount(model.catList[i]);
 					break;
 				}
 			}
 		});
+	},
+
+	renderClickCount: function(cat){
+		clickCount.innerHTML = cat.clickNumber;
+	},
+
+	listRender: function(){
+		var catButtons = controller.buttonReady(model.catList);
+		var catButtonList = controller.createUlList(catButtons);
+		this.insertList(catButtonList, this.catButtons);
+	},
+
+	imageAndClickRender: function(){
+		view.createButtonHandlers(model.catList);
+		view.createImageEventListeners(view.catImage);
 	}
+
+
 };
 
+//initial cats
+model.addCat("Fred", "images/cat.jpg");
+model.addCat("Mittens", "images/cat2.jpg");
+model.addCat("Belleh", "images/cat3.jpg");
+model.addCat("Cotton", "images/cat4.jpg");
+model.addCat("William", "images/cat5.jpg");
 
-
-
-
-/*
-
-//array of button-ready markup for each cat in catList
-var catButtons = buttonReady(catList);
-
-//html list of cat buttons
-catButtonList = createUlList(catButtons);
-
-//list inserted inside div with id of "catButtons"
-insertList(catButtonList, 'catButtons');
-
-var clickCount = document.getElementById("clickCount");
-var catImage = document.getElementsByClassName("catImage");
-catImage = catImage[0];
-
-createButtonHandlers(catList);
-
-*/
-
-controller.addCat("Fred", "images/cat.jpg");
-controller.addCat("Fred", "images/cat.jpg");
-controller.addCat("Mittens", "images/cat2.jpg");
-controller.addCat("Belleh", "images/cat3.jpg");
-controller.addCat("Cotton", "images/cat4.jpg");
-controller.addCat("William", "images/cat5.jpg");
+//start program
+controller.init();
